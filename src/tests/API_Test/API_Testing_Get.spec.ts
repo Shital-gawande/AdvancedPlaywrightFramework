026676d510e68eb5e0c943c4da1e2bac36afb222
Shital Gawande <shitalgawande01@gmail.com>
2026-06-14 14:09:02 +0530

    Adding get API with path paramter and json response validation

---
+++ src/tests/API_Test/API_Testing_Get.spec.ts
@@ -0,0 +1,19 @@
+//get method with path parameter and validating the json response body
+import { test, expect } from '@playwright/test';
+
+test('GetBooking API', async ({ request }) => {
+    //normal way to get the response body
+    // const response = await request.get('https://restful-booker.herokuapp.com/booking/1');
+    const bookID = 12;
+    const response = await request.get(`https://restful-booker.herokuapp.com/booking/${bookID}`);
+    expect(response.status()).toBe(200);
+
+    const jsonResponse = await response.json();
+
+    expect(jsonResponse.firstname).toBe('Jane');
+    expect(jsonResponse.lastname).toBe('Doe');
+    expect(jsonResponse.totalprice).toBe(111);
+    expect(jsonResponse.depositpaid).toBe(true);
+    expect(jsonResponse.bookingdates.checkin).toBe('2018-01-01');
+    expect(jsonResponse.bookingdates.checkout).toBe('2019-01-01');
+});
